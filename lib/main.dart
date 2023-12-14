4b964e42e8b58a6987fc4fc9109654470a50e246
deadpool3010 <110459725+deadpool3010@users.noreply.github.com>
2023-12-15 01:47:32 +0530

    fiest commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'dart:io';
 import 'package:expenses/sqlhelper.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
+
+TextEditingController textEditingController = new TextEditingController();
 
 void main() {
   runApp(MyApp());
@@ -23,81 +21,83 @@ class MyApp extends StatelessWidget {
 }
 
 class TodoListScreen extends StatefulWidget {
+  const TodoListScreen({super.key});
+
   @override
-  _TodoListScreenState createState() => _TodoListScreenState();
+  State<TodoListScreen> createState() => _TodoListScreenState();
 }
 
+@override
 class _TodoListScreenState extends State<TodoListScreen> {
-  final DbHelper dbHelper = DbHelper();
-  List<Todo> todos = [];
-  TextEditingController _textEditingController = TextEditingController();
-
   @override
   void initState() {
     super.initState();
-    _loadData();
+    loadData();
   }
 
-  void _loadData() async {
+  final DbHelper dbHelper = new DbHelper();
+  List<Todo> todos = [];
+
+  // Step 1
+
+  void loadData() async {
     await dbHelper.initdb();
-    List<Map<String, dynamic>> data = await dbHelper.read();
+    List<Map<String, dynamic>> list = await dbHelper.read();
     setState(() {
-      todos = data.map((item) => Todo.fromMap(item)).toList();
+      todos = list.map((item) => Todo.fromMap(item)).toList();
     });
   }
+  // Step 2 in step two we have to inset our data in todo and use insert function
 
-  void _addTodo() async {
-    String title = _textEditingController.text;
+  void addtodo() async {
+    String title = textEditingController.text; // Use text instead of toString
     if (title.isNotEmpty) {
-      Todo newTodo = Todo(
-        title: title,
-        completed: false,
-      );
-      await dbHelper.insert(newTodo.toMap());
-      _textEditingController.clear();
-      _loadData();
+      // Check if title is not empty
+      Todo newtodo = Todo(title: title, completed: false);
+      await dbHelper.insert(newtodo.toMap());
+      loadData();
+      textEditingController.clear();
     }
   }
 
-  void _deleteTodo(int id) async {
-    await dbHelper.delete(id);
-    _loadData();
+  void delete(int? id) async {
+    await dbHelper.delete(id!);
+    loadData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Todo List'),
+        title: Text('Todo app'),
       ),
       body: Column(
         children: [
           TextField(
-            controller: _textEditingController,
-            decoration: InputDecoration(
-              hintText: 'Enter a new todo',
-              contentPadding: EdgeInsets.all(16.0),
-            ),
+            decoration: InputDecoration(hintText: 'Enter title'),
+            controller: textEditingController,
           ),
           ElevatedButton(
-            onPressed: _addTodo,
-            child: Text('Add Todo'),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: todos.length,
-              itemBuilder: (context, index) {
-                Todo todo = todos[index];
-                return ListTile(
-                  title: Text(todo.title ?? ''),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () => _deleteTodo(todo.id ?? 0),
-                  ),
-                );
+              onPressed: () {
+                addtodo();
+                print(todos);
               },
-            ),
-          ),
+              child: Text('+')),
+          Expanded(
+              child: ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              Todo todo = todos[index];
+              return ListTile(
+                title: Text(todo.title ?? ''),
+                trailing: IconButton(
+                    onPressed: () {
+                      delete(todo.id);
+                    },
+                    icon: const Icon(Icons.delete)),
+              );
+            },
+          ))
         ],
       ),
     );
